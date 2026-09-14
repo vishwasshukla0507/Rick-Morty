37e86a2051b255c3be6e9391634c484a0f63a610
Vishwas Shukla <vishwasshukla0507@gmail.com>
2026-09-14 19:40:25 +0530

    Initial Commit

---
+++ Rick & Morty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rick & Morty
+//
+//  Created by Vishwas Shukla on 14/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
